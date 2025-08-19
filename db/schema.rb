627f905e4f7fde398514d9af6e73b985d5ccb12b
Kanyorok <rmjesusaddict@gmail.com>
2025-08-19 18:26:19 +0300

    Fixed the schema

--- db/schema.rb
+++ db/schema.rb
@@ -133,16 +133,11 @@ ActiveRecord::Schema[7.2].define(version: 2025_08_19_064138) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.uuid "created_by_id"
-    t.uuid "modified_by_id"
-    t.uuid "deleted_by_id"
+    t.uuid "created_by", default: "c5d5cc2c-5ab2-4301-811a-5b6e8e4f61da", null: false
+    t.uuid "modified_by", default: "c5d5cc2c-5ab2-4301-811a-5b6e8e4f61da", null: false
+    t.uuid "deleted_by"
     t.datetime "deleted_on"
-    t.boolean "archive_status", default: false, null: false
-    t.index ["archive_status"], name: "index_banking_types_on_archive_status"
-    t.index ["created_by_id"], name: "index_banking_types_on_created_by_id"
-    t.index ["deleted_by_id"], name: "index_banking_types_on_deleted_by_id"
     t.index ["deleted_on"], name: "index_banking_types_on_deleted_on"
-    t.index ["modified_by_id"], name: "index_banking_types_on_modified_by_id"
   end
 
   create_table "boards", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
@@ -546,16 +541,11 @@ ActiveRecord::Schema[7.2].define(version: 2025_08_19_064138) do
     t.uuid "parent_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.uuid "created_by_id"
-    t.uuid "modified_by_id"
-    t.uuid "deleted_by_id"
+    t.uuid "created_by", default: "c5d5cc2c-5ab2-4301-811a-5b6e8e4f61da", null: false
+    t.uuid "modified_by", default: "c5d5cc2c-5ab2-4301-811a-5b6e8e4f61da", null: false
+    t.uuid "deleted_by"
     t.datetime "deleted_on"
-    t.boolean "archive_status", default: false, null: false
-    t.index ["archive_status"], name: "index_qa_modules_on_archive_status"
-    t.index ["created_by_id"], name: "index_qa_modules_on_created_by_id"
-    t.index ["deleted_by_id"], name: "index_qa_modules_on_deleted_by_id"
     t.index ["deleted_on"], name: "index_qa_modules_on_deleted_on"
-    t.index ["modified_by_id"], name: "index_qa_modules_on_modified_by_id"
     t.index ["parent_id"], name: "index_qa_modules_on_parent_id"
   end
 
@@ -902,9 +892,6 @@ ActiveRecord::Schema[7.2].define(version: 2025_08_19_064138) do
   add_foreign_key "addusers", "users"
   add_foreign_key "assignees", "projects"
   add_foreign_key "assignees", "users"
-  add_foreign_key "banking_types", "users", column: "created_by_id"
-  add_foreign_key "banking_types", "users", column: "deleted_by_id"
-  add_foreign_key "banking_types", "users", column: "modified_by_id"
   add_foreign_key "boards", "products"
   add_foreign_key "boards", "users"
   add_foreign_key "clients", "users"
@@ -940,9 +927,6 @@ ActiveRecord::Schema[7.2].define(version: 2025_08_19_064138) do
   add_foreign_key "projects", "groupwares"
   add_foreign_key "projects", "softwares"
   add_foreign_key "projects", "users"
-  add_foreign_key "qa_modules", "users", column: "created_by_id"
-  add_foreign_key "qa_modules", "users", column: "deleted_by_id"
-  add_foreign_key "qa_modules", "users", column: "modified_by_id"
   add_foreign_key "ratings", "tickets"
   add_foreign_key "ratings", "users"
   add_foreign_key "scripts", "groupwares"
