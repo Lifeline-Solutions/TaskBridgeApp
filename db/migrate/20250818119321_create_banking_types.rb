ba79cf2caebab6103afe89c88359622c9af2b8b8
ger619 <abolger254@gmail.com>
2025-08-20 10:44:00 +0300

    Add `if_not_exists` option to `create_table` in `CreateBankingTypes` migration for idempotency.

--- db/migrate/20250818119321_create_banking_types.rb
+++ db/migrate/20250818119321_create_banking_types.rb
@@ -1,6 +1,6 @@
 class CreateBankingTypes < ActiveRecord::Migration[7.2]
   def change
-    create_table :banking_types, id: :uuid, default: -> { "gen_random_uuid()" } do |t|
+    create_table :banking_types, id: :uuid, default: -> { "gen_random_uuid()" }, if_not_exists: true do |t|
       t.string :name, null: false
       t.timestamps
     end
