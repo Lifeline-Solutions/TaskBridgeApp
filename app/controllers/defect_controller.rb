4eb15a3788816329cf33a7df63a244ab72119c98
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2025-08-22 12:37:15 +0300

    Add functionality to ensure that the issue type added to the defect saves as needed

--- app/controllers/defect_controller.rb
+++ app/controllers/defect_controller.rb
@@ -59,7 +59,11 @@ class DefectController < ApplicationController
     @qa_modules = QaModule.where(parent_id: nil)
     @banking_types = BankingType.all
     @products = Product.with_quality_assurance_status
-    @statuses = Status.all
+    @statuses = Status.where(name: [
+      'To Do', 'In Progress', 'On hold', 'Awaiting client info', 
+      'Awaiting build', 'QA testing', 'Closed', 'Failed QA', 
+      'Blocked', 'Reopened'
+    ])
     @users = User.with_agent_project_manager_role.order(:first_name, :last_name)
     @submodules = @defect.qa_module ? @defect.qa_module.submodules : []
 
@@ -218,6 +222,7 @@ class DefectController < ApplicationController
       :banking_type_id,
       :priority,
       :product_id,
+      :issue_type,
       :defect_unique,
       user_ids: []
     )
