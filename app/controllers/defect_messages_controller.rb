8c714e104398c71aae5a973606d2ab49d6df48c3
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2025-08-25 15:56:28 +0300

    Ensure proper edit functionality of the defect messages

--- app/controllers/defect_messages_controller.rb
+++ app/controllers/defect_messages_controller.rb
@@ -58,10 +58,7 @@ class DefectMessagesController < ApplicationController
     audit_on_update(@defect_message)
 
     if @defect_message.update(defect_message_params)
-      respond_to do |format|
-        format.turbo_stream
-        format.html { redirect_to @defect, notice: "Message updated successfully." }
-      end
+      redirect_to defect_path(@defect), notice: "Message updated successfully."
     else
       render :edit, status: :unprocessable_entity
     end
