9a86097710f40299ea11f230ce7e484c9bc14d4e
Briton Otieno <ojwangbriton@gmail.com>
2025-08-26 14:21:49 +0300

    Expand permissions for certain roles.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -6,16 +6,21 @@ class Ability
     if user.has_role? :admin
       can :manage, :all # allow super admins to do anything
       can :generate, :report
+
     elsif user.has_role? :observer
       can :read, :all
       can %i[generate report cease_fire_report email_report user_report breach_report user_report], :report
-      can %i[add_status add_status update_issue_type update_due_date update_priority index_home all_tickets], Ticket
+      can %i[add_status add_status update_issue_type update_due_date update_priority index_home all_tickets all_tickets_created_by_inactive_team_members all_tickets_created_by_inactive_users],
+          Ticket
       can :manage, Issue, user_id: user.id
+
     elsif user.has_role? :ceo
       can :generate, :report # allow ceo to do anything cept manage all
+
     elsif user.has_role?('project manager')
       can %i[read assign_user unassign_user add_team manage_users], Project
-      can %i[create read assign_tag unassign_tag add_status update_issue_type update_due_date update_priority index_home all_tickets modal_show], Ticket
+      can %i[create read assign_tag unassign_tag add_status update_issue_type update_due_date update_priority index_home all_tickets modal_show all_tickets_created_by_inactive_team_members all_tickets_created_by_inactive_users],
+          Ticket
       can %i[edit destroy update], Ticket, user_id: user.id
       can :manage, Issue, user_id: user.id
       can %i[create read add_user remove_user edit update manage_users product_status], Product
@@ -26,6 +31,7 @@ class Ability
       can :manage, Task
       can :generate, :report
       can :manage, Team
+
     elsif user.has_role? :agent
       can %i[read assign_user unassign_user add_team manage_users], Project
       can %i[create read assign_tag unassign_tag update_status add_status index_home all_tickets modal_show], Ticket
@@ -40,6 +46,7 @@ class Ability
       can :manage, Message
       can :manage, Defect
       can :generate, :report
+
     elsif user.has_role? :client
       can :read, Project
       can %i[create read assign_tag unassign_tag update_status add_status index_home all_tickets modal_show], Ticket
@@ -51,6 +58,7 @@ class Ability
       cannot :manage, Task
       can :manage, Message
       can :generate, :report
+
     else
       can :read, Project
       can :read, Ticket
