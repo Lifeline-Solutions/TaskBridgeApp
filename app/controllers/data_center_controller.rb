14d14a1d05712c8df713404ceefd54758987085d
ger619 <abolger254@gmail.com>
2025-08-26 10:51:11 +0300

    Morning Tickets with Active users only and end of day with active users only

--- app/controllers/data_center_controller.rb
+++ app/controllers/data_center_controller.rb
@@ -518,7 +518,7 @@ class DataCenterController < ApplicationController
 
     base_scope = Ticket.joins(:users, project: :client)
       .joins(:statuses, :taggings)
-      .where(users: { id: user_ids })
+      .where(users: { id: user_ids, active: true })
       .where.not(statuses: { name: outstanding_statuses })
 
     team.users.each do |user|
