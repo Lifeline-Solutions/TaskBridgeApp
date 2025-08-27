2c04e5c936898e0ce64a2950773a56a32bf973f0
ger619 <abolger254@gmail.com>
2025-08-27 11:50:04 +0300

    Defect history added to defect

--- app/models/status.rb
+++ app/models/status.rb
@@ -2,7 +2,7 @@ class Status < ApplicationRecord
   has_many :add_statuses
   has_many :tickets, through: :add_statuses, dependent: :destroy
   has_many :status_bugs
-  has_and_belongs_to_many :defects, join_table: :defect_statuses
+  has_and_belongs_to_many :defects, join_table: :defect_statuses, dependent: :destroy
   has_many :bugs, through: :status_bugs, dependent: :destroy
   has_and_belongs_to_many :products, dependent: :destroy
   has_and_belongs_to_many :tasks, dependent: :destroy
