b35c2eb0b6836f7f716a5f0af744783cf635ed97
ger619 <abolger254@gmail.com>
2025-08-28 12:11:19 +0300

    Ability change to ensure users are able to get the details

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,8 +1,5 @@
-# frozen_string_literal: true
-
-# Only initialize Sentry when a DSN is provided.
 Sentry.init do |config|
-  config.dsn = 'https://2c85eaaa604d06def24a5250f6595b55@o4507601057808384.ingest.de.sentry.io/4509908884783184'
+  config.dsn = 'https://30e370340976cf8c95c0bbd2c4ae2ee8@o4507601057808384.ingest.de.sentry.io/4509908898480208'
   config.breadcrumbs_logger = [:active_support_logger, :http_logger]
 
   # Add data like request headers and IP for users,
