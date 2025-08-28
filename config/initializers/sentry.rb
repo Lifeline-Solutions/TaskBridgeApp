038e2b4af3a3144763bbd32939482f56c6abe361
Kanyorok <rmjesusaddict@gmail.com>
2025-08-28 12:11:46 +0300

    Hotfix the sentry error initialization

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,11 +1,5 @@
-# frozen_string_literal: true
-
-# Only initialize Sentry when a DSN is provided.
 Sentry.init do |config|
-  config.dsn = 'https://2c85eaaa604d06def24a5250f6595b55@o4507601057808384.ingest.de.sentry.io/4509908884783184'
+  config.dsn = ENV['SENTRY_DSN'] # Only initializes if ENV is set
   config.breadcrumbs_logger = [:active_support_logger, :http_logger]
-
-  # Add data like request headers and IP for users,
-  # see https://docs.sentry.io/platforms/ruby/data-management/data-collected/ for more info
   config.send_default_pii = true
-end
+end if ENV['SENTRY_DSN'].present?
