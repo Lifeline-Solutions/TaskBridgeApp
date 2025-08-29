87ca29adea20416c80c5c52a83e9664b0f4ac358
Briton Otieno <ojwangbriton@gmail.com>
2025-08-29 16:03:11 +0300

    Add product association

--- app/models/qa_module.rb
+++ app/models/qa_module.rb
@@ -1,4 +1,5 @@
 class QaModule < ApplicationRecord
+  belongs_to :product
   belongs_to :parent, class_name: 'QaModule', optional: true
   has_many :submodules, class_name: 'QaModule', foreign_key: 'parent_id', dependent: :destroy
 
