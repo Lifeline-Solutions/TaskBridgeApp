b3b7f470eec67e94bd938be15fe2c28ea7e46276
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2025-08-29 12:37:17 +0300

    On defect update, do not dleete those that existed before

--- app/controllers/defect_controller.rb
+++ app/controllers/defect_controller.rb
@@ -113,35 +113,27 @@ class DefectController < ApplicationController
   def update
     audit_on_update(@defect)
 
-    # Collect files from either place (prefer model-scoped)
-    files = []
-    files += Array(params.dig(:defect, :attachments)).reject(&:blank?) if params.dig(:defect, :attachments).present?
-    files += Array(params[:attachments]).reject(&:blank?) if params[:attachments].present?
+    selected_user_ids = params[:defect][:user_ids]
 
-    files.each { |file| @defect.attachments.attach(file) } if files.any?
+    if @defect.update(defect_params.except(:attachments))
+      # Attach new files without removing old ones
+      if params[:defect][:attachments].present?
+        params[:defect][:attachments].each do |file|
+          @defect.attachments.attach(file)
+        end
+      end
 
-    selected_user_ids = params[:defect][:user_ids] # This will be an array of user IDs
+      @defect.user_ids = selected_user_ids
 
-    if @defect.update(defect_params)
       activity('user_activity')
         .caused_by(current_user)
         .performed_on(@defect)
         .event('defect.update')
-        .with_properties(
-          defect_id: @defect.id,
-          qa_module_id: @defect.qa_module_id # optional, only if you want this info
-        )
         .log("Updated Defect ##{@defect.id}")
-      @defect.user_ids = selected_user_ids
 
       redirect_to @defect, notice: 'Defect was successfully updated.'
-      assigned_names = @defect.users.map { |u| "#{u.first_name} #{u.last_name}" }.join(', ')
-      log_event(
-        @defect, current_user, 'Updated and Assigned',
-        assigned_names.present? ? "Defect was updated and assigned to #{assigned_names} at #{Time.now.strftime('%H:%M of  %d-%m-%Y')}" : "Defect was Updated but no assigned user at #{Time.now.strftime('%H:%M of  %d-%m-%Y')}"
-      )
     else
-      render :edit
+      render :edit, status: :unprocessable_entity
     end
   end
 
