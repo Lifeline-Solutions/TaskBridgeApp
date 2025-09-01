719690e90811972ffe9a44d195f63127b17078f8
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2025-09-01 14:54:43 +0300

    update included:   - Update the controller to include the functionality for saving a draft defect   - In the create UI, add the button for saving as draft

--- app/controllers/defect_controller.rb
+++ app/controllers/defect_controller.rb
@@ -75,26 +75,29 @@ class DefectController < ApplicationController
   def create
     @defect = Defect.new(defect_params)
     @defect.creator = current_user
-    selected_user_ids = params[:defect][:user_ids] # This will be an array of user IDs
+    selected_user_ids = params[:defect][:user_ids]
 
     if @defect.save
       @defect.user_ids = selected_user_ids
-      activity('user_activity')
-        .caused_by(current_user)
-        .performed_on(@defect)
-        .event('defect.create')
-        .with_properties(
-          defect_attributes: @defect.attributes,
-          assigned_user_ids: selected_user_ids # <-- Add to log properties
-        )
-        .log("Created Defect ##{@defect.id}, assigned to User IDs: #{selected_user_ids.join(', ')}")
 
-      redirect_to defect_index_path, notice: 'Defect was successfully created.'
-      assigned_names = @defect.users.map { |u| "#{u.first_name} #{u.last_name}" }.join(', ')
-      log_event(
-        @defect, current_user, 'Created and Assigned',
-        assigned_names.present? ? "Defect was created and assigned to #{assigned_names} at #{Time.now.strftime('%H:%M of  %d-%m-%Y')}" : "Defect was created but no assigned user at #{Time.now.strftime('%H:%M of  %d-%m-%Y')}"
-      )
+      if @defect.draft?
+        redirect_to defect_index_path, notice: "Draft defect saved successfully."
+      else
+        activity('user_activity')
+          .caused_by(current_user)
+          .performed_on(@defect)
+          .event('defect.create')
+          .with_properties(defect_attributes: @defect.attributes, assigned_user_ids: selected_user_ids)
+          .log("Created Defect ##{@defect.id}, assigned to User IDs: #{selected_user_ids.join(', ')}")
+
+        assigned_names = @defect.users.map { |u| "#{u.first_name} #{u.last_name}" }.join(', ')
+        log_event(
+          @defect, current_user, 'Created and Assigned',
+          assigned_names.present? ? "Defect was created and assigned to #{assigned_names} at #{Time.now.strftime('%H:%M of  %d-%m-%Y')}" : "Defect was created but no assigned user at #{Time.now.strftime('%H:%M of  %d-%m-%Y')}"
+        )
+
+        redirect_to defect_index_path, notice: 'Defect was successfully created.'
+      end
     else
       set_form_data
       flash.now[:alert] = "Defect creation failed: #{@defect.errors.full_messages.join(', ')}"
@@ -215,6 +218,11 @@ class DefectController < ApplicationController
       )
     end
   end
+
+  def drafts
+    @defects = Defect.drafts.includes(:users, :qa_module, :submodule).order(updated_at: :desc)
+  end
+
 
   def defect_status
     @defect = Defect.find(params[:id])
@@ -385,6 +393,7 @@ class DefectController < ApplicationController
       :banking_type_id,
       :priority,
       :label,
+      :draft,
       :product_id,
       :issue_type,
       :defect_unique,
