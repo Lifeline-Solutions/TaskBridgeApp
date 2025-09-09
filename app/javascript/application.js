9537a0acd7e6bb8b0690feafa8f818b07e81ea31
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2025-09-09 11:06:53 +0300

    Remove the tomselect init js file upon investigation it is not being used

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -8,8 +8,6 @@ import 'trix';
 import '@rails/actiontext';
 
 /* import './controllers/ckeditor_init'
-import './controllers/label_tomselect_init'
-
  */
 
 
@@ -20,7 +18,3 @@ import './controllers/label_tomselect_init'
 //= require 'chart.js'
 // import "./channels"
 //= require 'trix_custom_formatting_controller.js'
-
-
-import "trix"
-import "@rails/actiontext"
