e8a2fcc9e609dcae3d1e56df47db377841c13ae2
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2025-09-09 15:19:14 +0300

    Fix JS unused code

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -7,9 +7,7 @@ import 'controllers';
 import 'trix';
 import '@rails/actiontext';
 
-/* import './controllers/ckeditor_init'
- */
-
+import './controllers/ckeditor_init'
 
 //= require rails-ujs
 //= require turbolinks
