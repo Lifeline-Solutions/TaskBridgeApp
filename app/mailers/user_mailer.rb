9fc1a03d3f5e8e4be98247fbda6f7f86fc16a7c5
ger619 <abolger254@gmail.com>
2025-09-10 12:04:55 +0300

    Update task assignment email to include unique task ID and correct URL generation

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -77,7 +77,7 @@ class UserMailer < ApplicationMailer
     @task = task
     @current_user = current_user
     @assigned_user = assigned_user
-    @url = product_tasks_url(@task.product, @task)
+    @url = product_task_url(@task.product, @task)
     mail(to: @user.email, subject: 'You have been assigned to a new task')
   end
 
