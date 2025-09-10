6f66ee7859a251edbd647a5749889b042f374199
ger619 <abolger254@gmail.com>
2025-09-10 08:29:13 +0300

    Add unique_task_id column to tasks table with unique index

--- db/migrate/20250910051707_add_unique_task_id_to_task.rb
+++ db/migrate/20250910051707_add_unique_task_id_to_task.rb
@@ -1,6 +1,6 @@
 class AddUniqueTaskIdToTask < ActiveRecord::Migration[7.2]
   def change
-    add_column :tasks, :unique_task_id, :string, null: false,  if_not_exists: true
+    add_column :tasks, :unique_task_id, :string, null: true,  if_not_exists: true
     add_index :tasks, :unique_task_id, unique: true, if_not_exists: true
   end
 end
