31d45414b91d3387c8a2e0fcec85b3089390b613
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2025-09-10 16:17:14 +0300

    Update included:   - Created the Defect failure reports comtroller wiht the needed actions   - Created a defect record service that helps to encapsulate the functionality of managing the different actions when failed_qa state is reached   - Custom service updates the counter of the number a defect went into QA failed

---
+++ app/controllers/defect_failure_reports_controller.rb
@@ -0,0 +1,36 @@
+class DefectFailureReportsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_defect
+  before_action :authorize_view!
+
+  def index
+    @reports = @defect.defect_failure_reports.visible.order(retest_number: :asc)
+    respond_to do |format|
+      format.html
+      format.json { render json: @reports.as_json(only: %i[id retest_number captured_at created_at]) }
+    end
+  end
+
+  def show
+    @report = @defect.defect_failure_reports.find(params[:id])
+  end
+
+  private
+
+  def set_defect
+    if params[:defect_id].present?
+      @defect = Defect.find(params[:defect_id])
+    elsif params[:id].present?
+      report = DefectFailureReport.find_by(id: params[:id])
+      @defect = report&.defect
+    else
+      head :bad_request
+    end
+  end
+
+  def authorize_view!
+    unless current_user.has_role?(:qa) || current_user.has_role?(:hod) || current_user.has_role?(:admin)
+      render plain: "Not authorized", status: :forbidden
+    end
+  end
+end
