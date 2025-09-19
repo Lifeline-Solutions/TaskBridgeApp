e72ed4c29dca42e44dd69c7c6a2e41037e92b335
ger619 <abolger254@gmail.com>
2025-09-19 14:21:44 +0300

    Refactor schema by removing unused tables and updating action mailer URL configuration

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -77,10 +77,8 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
 
   # Raise error when a before_action's only/except options reference missing actions
-  config.action_mailer.default_url_options = { host: 'craftsilicon.com', protocol: 'http' }
+  config.action_mailer.default_url_options = { host: 'taskbridge.craftsilicon.com', protocol: 'https' }
   config.action_controller.raise_on_missing_callback_actions = true
-  config.action_mailer.default_url_options = { host: 'http://172.16.2.15' }
-  config.active_storage.variant_processor = :mini_magick
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :smtp
