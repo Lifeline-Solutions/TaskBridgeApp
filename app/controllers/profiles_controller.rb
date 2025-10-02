471a964eb540a8f7adcf60130794cdd2a8bd1280
ger619 <abolger254@gmail.com>
2025-10-02 15:33:00 +0300

    Refactor ticket chart data processing to remove excluded statuses logic

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -52,16 +52,12 @@ class ProfilesController < ApplicationController
         hash[user_id][status] = count
       end
 
-      # excluded_statuses = %w[Closed Resolved Declined]
-      # filtered_chart_data = @organized_tickets.transform_values do |data|
-      #  filtered = data.reject { |k, _| excluded_statuses.include?(k) || k == :total }
-      #   filtered.values.sum
-      # end
+      excluded_statuses = %w[Closed Resolved Declined]
+       filtered_chart_data = @organized_tickets.transform_values do |data|
+        filtered = data.reject { |k, _| excluded_statuses.include?(k) || k == :total }
+         filtered.values.sum
+       end
 
-      filtered_chart_data = @organized_tickets.transform_values do |data|
-        filtered = data.except(:total)
-        filtered.values.sum
-      end
 
       @tickets_chart_data = filtered_chart_data.transform_keys { |id| User.find(id).name }
       @tickets_per_project = @tickets
