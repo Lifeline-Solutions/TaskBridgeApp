3f2baeeff2f931986343d77ced176afb14100895
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2025-10-07 12:07:02 +0300

    Add additional fields to the defect_filters adding filter_type and enforcing uniqueness constraints

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_10_03_045838) do
+ActiveRecord::Schema[7.2].define(version: 2025_10_07_090136) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -28,18 +28,6 @@ ActiveRecord::Schema[7.2].define(version: 2025_10_03_045838) do
     t.datetime "deleted_on"
     t.index ["deleted_on"], name: "index_action_text_rich_texts_on_deleted_on"
     t.index ["record_type", "record_id", "name"], name: "index_action_text_rich_texts_uniqueness", unique: true
-  end
-
-  create_table "action_text_tables", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
-    t.jsonb "content", default: [["", ""], ["", ""]], null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "action_text_tables_table", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
-    t.jsonb "content", default: [["", ""], ["", ""]], null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "active_storage_attachments", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
@@ -160,6 +148,14 @@ ActiveRecord::Schema[7.2].define(version: 2025_10_03_045838) do
     t.index ["product_id"], name: "index_banking_types_on_product_id"
   end
 
+  create_table "banking_types_products", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.uuid "product_id", null: false
+    t.uuid "banking_type_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["product_id", "banking_type_id"], name: "index_banking_types_products_on_product_and_banking_type", unique: true
+  end
+
   create_table "boards", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "status"
     t.uuid "product_id", null: false
@@ -278,13 +274,19 @@ ActiveRecord::Schema[7.2].define(version: 2025_10_03_045838) do
     t.boolean "archive_status", default: false, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "filter_type", default: "defect", null: false
+    t.jsonb "chart_config", default: {}
+    t.boolean "is_dashboard", default: false, null: false
     t.index ["archive_status"], name: "index_defect_filters_on_archive_status"
     t.index ["created_by_id"], name: "index_defect_filters_on_created_by_id"
     t.index ["deleted_by_id"], name: "index_defect_filters_on_deleted_by_id"
     t.index ["deleted_on"], name: "index_defect_filters_on_deleted_on"
+    t.index ["filter_type"], name: "index_defect_filters_on_filter_type"
     t.index ["filters"], name: "index_defect_filters_on_filters", using: :gin
+    t.index ["is_dashboard"], name: "index_defect_filters_on_is_dashboard"
     t.index ["modified_by_id"], name: "index_defect_filters_on_modified_by_id"
     t.index ["product_id"], name: "index_defect_filters_on_product_id"
+    t.index ["user_id", "name", "filter_type"], name: "index_user_name_filter_type_unique", unique: true
     t.index ["user_id"], name: "index_defect_filters_on_user_id"
   end
 
@@ -340,7 +342,6 @@ ActiveRecord::Schema[7.2].define(version: 2025_10_03_045838) do
     t.uuid "deleted_by_id"
     t.datetime "deleted_on"
     t.boolean "archive_status", default: false, null: false
-    t.text "content"
     t.index ["archive_status"], name: "index_defect_messages_on_archive_status"
     t.index ["created_by_id"], name: "index_defect_messages_on_created_by_id"
     t.index ["defect_id"], name: "index_defect_messages_on_defect_id"
@@ -380,10 +381,8 @@ ActiveRecord::Schema[7.2].define(version: 2025_10_03_045838) do
     t.string "summary", null: false
     t.string "defect_unique"
     t.string "issue_type", default: "Bug"
-    t.text "labels", default: [], array: true
     t.boolean "draft", default: false, null: false
     t.integer "retest_count", default: 0, null: false
-    t.boolean "linkage_blocked", default: false, null: false
     t.index ["banking_type_id"], name: "index_defects_on_banking_type_id"
     t.index ["creator_id"], name: "index_defects_on_creator_id"
     t.index ["defect_unique"], name: "index_defects_on_defect_unique", unique: true
@@ -641,21 +640,6 @@ ActiveRecord::Schema[7.2].define(version: 2025_10_03_045838) do
     t.index ["script_id"], name: "index_products_on_script_id"
     t.index ["software_id"], name: "index_products_on_software_id"
     t.index ["user_id"], name: "index_products_on_user_id"
-  end
-
-  create_table "products_qa_modules", id: false, force: :cascade do |t|
-    t.uuid "product_id", null: false
-    t.uuid "qa_module_id", null: false
-    t.uuid "created_by", default: "c5d5cc2c-5ab2-4301-811a-5b6e8e4f61da"
-    t.uuid "modified_by", default: "c5d5cc2c-5ab2-4301-811a-5b6e8e4f61da"
-    t.uuid "deleted_by"
-    t.datetime "deleted_on"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["deleted_on"], name: "index_products_qa_modules_on_deleted_on"
-    t.index ["product_id", "qa_module_id"], name: "index_products_qa_modules_on_product_id_and_qa_module_id", unique: true
-    t.index ["product_id"], name: "index_products_qa_modules_on_product_id"
-    t.index ["qa_module_id"], name: "index_products_qa_modules_on_qa_module_id"
   end
 
   create_table "products_scripts", id: false, force: :cascade do |t|
@@ -1106,6 +1090,8 @@ ActiveRecord::Schema[7.2].define(version: 2025_10_03_045838) do
   add_foreign_key "banking_types", "users", column: "created_by_id"
   add_foreign_key "banking_types", "users", column: "deleted_by_id"
   add_foreign_key "banking_types", "users", column: "modified_by_id"
+  add_foreign_key "banking_types_products", "banking_types"
+  add_foreign_key "banking_types_products", "products"
   add_foreign_key "boards", "products"
   add_foreign_key "boards", "users"
   add_foreign_key "clients", "users"
