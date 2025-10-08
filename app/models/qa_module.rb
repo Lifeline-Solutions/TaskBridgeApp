4f41f12a43b39f5609b4390b63b9ad8d0efb8dc8
Nemwel-Boniface <nemwelboniface@outlook.com>
2025-10-08 22:43:33 +0300

    Add custom methods that help check the parent or chil in the qa_modules

--- app/models/qa_module.rb
+++ app/models/qa_module.rb
@@ -1,8 +1,22 @@
 class QaModule < ApplicationRecord
   belongs_to :product
   belongs_to :parent, class_name: 'QaModule', optional: true
+  has_many :children, class_name: 'QaModule', foreign_key: 'parent_id', dependent: :destroy
   has_many :submodules, class_name: 'QaModule', foreign_key: 'parent_id', dependent: :destroy
   has_many :defects, dependent: :nullify
 
   validates :name, presence: true
+
+  # Scopes
+  scope :modules, -> { where(parent_id: nil) }
+  scope :submodules, -> { where.not(parent_id: nil) }
+
+  # Instance methods
+  def module?
+    parent_id.nil?
+  end
+  
+  def submodule?
+    parent_id.present?
+  end
 end
