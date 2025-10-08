34d20111da4b85310c00d087eb5434607379bfa7
ger619 <abolger254@gmail.com>
2025-10-08 11:20:06 +0300

    Add open_tasks association to product model and update task count display

--- app/models/product.rb
+++ app/models/product.rb
@@ -11,6 +11,7 @@ class Product < ApplicationRecord
   belongs_to :client
   has_many :boards, dependent: :destroy
   has_many :tasks, dependent: :destroy
+  has_many :open_tasks, -> { joins(:statuses).where.not(statuses: { name: %w[Closed Resolved Declined] }) }, class_name: 'Task'
   has_many :documents, dependent: :destroy
   accepts_nested_attributes_for :documents, allow_destroy: true
   has_one_attached :image
