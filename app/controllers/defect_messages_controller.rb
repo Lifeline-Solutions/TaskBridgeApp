660854e357a8481e850917dd5adea809af1b5acb
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2025-10-15 12:29:08 +0300

    Update the defect mesages controller and the model to show when a user makes an edit of a comment

--- app/controllers/defect_messages_controller.rb
+++ app/controllers/defect_messages_controller.rb
@@ -69,9 +69,14 @@ class DefectMessagesController < ApplicationController
     old_content = @defect_message.content.to_plain_text
     audit_on_update(@defect_message)
 
-    if @defect_message.update(defect_message_params)
+    if @defect_message.update(defect_message_params.merge(modified_by_id: current_user.id))
       # Log the message update in defect history
-      log_event(@defect, current_user, 'message_updated', "Message updated from: #{old_content.truncate(100)} to: #{@defect_message.content.to_plain_text.truncate(100)}")
+      log_event(
+        @defect,
+        current_user,
+        'message_updated',
+        "Message updated from: #{old_content.truncate(100)} to: #{@defect_message.content.to_plain_text.truncate(100)}"
+      )
 
       # Process mentions asynchronously for updated message - convert ActionText to HTML string for job serialization
       ProcessMentionsJob.perform_later(
--- app/models/defect_message.rb
+++ app/models/defect_message.rb
@@ -1,6 +1,7 @@
 class DefectMessage < ApplicationRecord
   belongs_to :defect
   belongs_to :user
+  belongs_to :modified_by, class_name: 'User', optional: true
   has_rich_text :content
 
   def record_type
