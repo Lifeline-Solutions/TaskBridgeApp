7972c94193246c1b58db611fd3b984e586f9f48d
Briton Otieno <ojwangbriton@gmail.com>
2025-10-24 11:31:11 +0300

    Revert "Merge pull request #361 from Kanyorok/reclone_fixes"
    This reverts commit 54927a4bdae6082f8199a46e43457d53ab630754, reversing
    changes made to cf645f0e02ffa2c052a4c2a931dd83f059535e91.

--- db/migrate/20250829085429_add_labels_to_defects.rb
+++ db/migrate/20250829085429_add_labels_to_defects.rb
@@ -1,7 +1,5 @@
 class AddLabelsToDefects < ActiveRecord::Migration[7.2]
   def change
-    unless column_exists?(:defects, :label)
-      add_column :defects, :label, :string
-    end
+    add_column :defects, :label, :string
   end
 end
