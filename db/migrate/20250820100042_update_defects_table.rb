5673accc1a7bd1bc4d8c119ddf4a920a3dc0b654
Briton Otieno <ojwangbriton@gmail.com>
2025-10-24 14:41:45 +0300

    Update @rails/actiontext and @rails/activestorage to version 8.1.0; upgrade datatables.net to version 2.3.4

--- db/migrate/20250820100042_update_defects_table.rb
+++ db/migrate/20250820100042_update_defects_table.rb
@@ -1,8 +1,11 @@
 class UpdateDefectsTable < ActiveRecord::Migration[7.2]
   def change
-    add_reference :defects, :product, type: :uuid, foreign_key: true, index: true
-    add_column :defects, :summary, :string, null: true
-    remove_column :defects, :status, :string
+    add_reference :defects, :product, type: :uuid, foreign_key: true, index: true, if_not_exists: true
+    add_column :defects, :summary, :string, null: true, if_not_exists: true
+    
+    # Remove the column only if it exists
+    remove_column :defects, :status if column_exists?(:defects, :status)
+    
     add_reference :defects, :status, type: :uuid, foreign_key: true, index: true
   end
 end
