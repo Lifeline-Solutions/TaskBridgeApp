9fbb7fe378cd4a75c5b646330de6d2741db3515e
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2025-10-28 16:35:22 +0300

    Add the if_not_exist clause for the defect indexes

--- db/migrate/20251028124053_add_module_sub_module_banking_type_to_defects.rb
+++ db/migrate/20251028124053_add_module_sub_module_banking_type_to_defects.rb
@@ -4,8 +4,8 @@ class AddModuleSubModuleBankingTypeToDefects < ActiveRecord::Migration[7.2]
     add_column :defects, :submodule_id, :uuid,  if_not_exists: true
     add_column :defects, :banking_type_id, :uuid,  if_not_exists: true
 
-    add_index :defects, :qa_module_id
-    add_index :defects, :submodule_id
-    add_index :defects, :banking_type_id
+    add_index :defects, :qa_module_id,  if_not_exists: true
+    add_index :defects, :submodule_id,  if_not_exists: true
+    add_index :defects, :banking_type_id,  if_not_exists: true
   end
 end
