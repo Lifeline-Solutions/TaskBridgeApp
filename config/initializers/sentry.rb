cf5bbeef8ea9e96466a499a5a6b04c7b8fa5cc79
ger619 <abolger254@gmail.com>
2025-10-30 08:26:41 +0300

    Update Sentry DSN and enhance data collection configuration

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,5 +1,8 @@
 Sentry.init do |config|
-  config.dsn = 'https://30e370340976cf8c95c0bbd2c4ae2ee8@o4507601057808384.ingest.de.sentry.io/4509908898480208'
+  config.dsn = 'https://484d650cacbee29caf07c89e2909f74b@o4507601057808384.ingest.de.sentry.io/4510276676485200'
   config.breadcrumbs_logger = [:active_support_logger, :http_logger]
+
+  # Add data like request headers and IP for users,
+  # see https://docs.sentry.io/platforms/ruby/data-management/data-collected/ for more info
   config.send_default_pii = true
-end if ENV['SENTRY_DSN'].present?
+end
