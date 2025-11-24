8c536695391086fbbe1f2f6819d5921bc49391e7
ger619 <abolger254@gmail.com>
2025-11-24 17:00:29 +0300

    Fix invitation email sender and subject for CEO role

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -10,19 +10,15 @@ Rails.application.configure do
 
   # Do not eager load code on boot.
   config.eager_load = false
-
   # Show full error reports.
   config.consider_all_requests_local = true
-
   # Enable server timing
   config.server_timing = true
-
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
   if Rails.root.join("tmp/caching-dev.txt").exist?
     config.action_controller.perform_caching = true
     config.action_controller.enable_fragment_cache_logging = true
-
     config.cache_store = :memory_store
     config.public_file_server.headers = {
       "Cache-Control" => "public, max-age=#{2.days.to_i}"
@@ -77,7 +73,6 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
 
   # Raise error when a before_action's only/except options reference missing actions
-
   config.action_mailer.default_url_options = { host: 'http://172.16.2.15', protocol: 'http' }
   config.action_controller.raise_on_missing_callback_actions = true
   config.active_storage.variant_processor = :mini_magick
