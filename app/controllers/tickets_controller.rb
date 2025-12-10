48d7b6edf6de885dc83fd7e9294812d07edb5e31
ger619 <abolger254@gmail.com>
2025-12-10 16:40:36 +0300

    feat: Add delete functionality for QA modules and child modules with confirmation prompt

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -269,7 +269,7 @@ class TicketsController < ApplicationController
         end
 
         # Set SLA for new feature or regular ticket
-        if @ticket.issue == 'NEW FEATURE'
+        if @ticket.issue == 'NEW FEATURE' or "BILLABLE FEATURE"
           SlaTicket.find_or_create_by!(ticket_id: @ticket.id) do |sla|
             sla.sla_status = 'NO SLA'
             sla.sla_target_response_deadline = 'NO SLA'
