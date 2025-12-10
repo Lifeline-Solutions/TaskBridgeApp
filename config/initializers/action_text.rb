15f4638b97124f011689eebc51c5b7b9698500e0
Briton Otieno <ojwangbriton@gmail.com>
2025-12-10 15:55:25 +0300

    feat: Allow table tags and `bgcolor` attribute in ActionText content.

--- config/initializers/action_text.rb
+++ config/initializers/action_text.rb
@@ -1,19 +1,29 @@
-# Configure ActionText and Rails sanitizer to preserve inline styles
+# Configure ActionText and Rails sanitizer to preserve inline styles and tables
 # This allows colors, highlights, tables, and other formatting to work properly
 
 Rails.application.config.to_prepare do
-  # Patch the Loofah scrubber that ActionText uses
-  # This preserves all CSS properties in style attributes
+  # Patch ActionText to allow table tags and style attributes
   module ActionText
     module ContentHelper
-      # Override sanitizer_allowed_attributes to include style
+      # Override allowed tags to include table elements
+      mattr_accessor :sanitizer_allowed_tags
+      self.sanitizer_allowed_tags = Set.new([
+        'action-text-attachment', 'a', 'aside', 'b', 'blockquote', 'br', 'caption', 'cite', 'code', 'del', 'details',
+        'div', 'dl', 'dt', 'dd', 'em', 'figcaption', 'figure', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'hr', 'i', 'img',
+        'li', 'mark', 'ol', 'p', 'pre', 'q', 's', 'samp', 'small', 'source', 'span', 'strike', 'strong', 'sub',
+        'summary', 'sup', 'time', 'u', 'ul', 'var', 'video',
+        # CRITICAL: Add table tags
+        'table', 'thead', 'tbody', 'tfoot', 'tr', 'th', 'td', 'colgroup', 'col'
+      ])
+
+      # Override allowed attributes to include style and table attributes
       mattr_accessor :sanitizer_allowed_attributes
       self.sanitizer_allowed_attributes = Set.new([
         'abbr', 'align', 'alt', 'axis', 'border', 'cellpadding', 'cellspacing', 'class', 'clear', 'cols', 'colspan', 'color',
         'compact', 'coords', 'dir', 'face', 'headers', 'height', 'hreflang', 'hspace', 'ismap', 'lang', 'longdesc',
         'name', 'nowrap', 'rel', 'rev', 'rows', 'rowspan', 'rules', 'scope', 'scrolling', 'shape', 'size', 'span',
         'start', 'summary', 'tabindex', 'target', 'title', 'type', 'usemap', 'valign', 'value', 'vspace', 'width',
-        'itemprop', 'href', 'src',
+        'itemprop', 'href', 'src', 'bgcolor',
         'style' # CRITICAL: Allow style attribute for colors and formatting
       ])
     end
