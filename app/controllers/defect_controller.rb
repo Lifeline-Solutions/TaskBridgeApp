69b3ac6d74afc65c79e8bd129047a78704b6b9e5
Kanyorok <rmjesusaddict@gmail.com>
2025-12-13 22:45:46 +0300

    feat: Dynamically join `qa_module` and `banking_type` for defect searches and resolve user IDs for `assignee` and `reporter` in dashboard drill-downs.

--- app/controllers/defect_controller.rb
+++ app/controllers/defect_controller.rb
@@ -424,9 +424,31 @@ class DefectController < ApplicationController
         # Skip if banking_types association not available
       end
 
-      # Use left_joins only if not already joined
-      @defects = @defects.left_joins(:users) unless @defects.to_sql.include?('INNER JOIN "users"')
-      @defects = @defects.left_joins(product: %i[client groupwares]).where(
+      # Use left_joins explicitly for searchable associations
+      @defects = @defects.left_joins(:users) unless @defects.to_sql.include?('JOIN "users"')
+      
+      # Always join product/client/groupwares as they are in the core search
+      @defects = @defects.left_joins(product: %i[client groupwares])
+
+      # Conditionally join qa_modules if we are searching it
+      begin
+        if search_conditions.any? { |c| c.include?('qa_modules.name') }
+           @defects = @defects.left_joins(:qa_module)
+        end
+      rescue StandardError
+        # Ignore if association doesn't exist
+      end
+
+      # Conditionally join banking_types if we are searching it
+      begin
+        if search_conditions.any? { |c| c.include?('banking_types.name') }
+           @defects = @defects.left_joins(:banking_type)
+        end
+      rescue StandardError
+        # Ignore if association doesn't exist
+      end
+
+      @defects = @defects.where(
         search_conditions.join(' OR '), q: q
       ).distinct
     end
