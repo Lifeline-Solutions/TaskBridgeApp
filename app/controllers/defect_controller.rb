2f9c39ee0ec993b3b011ed09679cc76c04a8fdf3
ger619 <abolger254@gmail.com>
2026-01-13 10:52:24 +0300

    fix: Update terminology from "Failed Retest Count" to "Failed Retest Percentage" in QA dashboard

--- app/controllers/defect_controller.rb
+++ app/controllers/defect_controller.rb
@@ -595,6 +595,7 @@ class DefectController < ApplicationController
     @start_count = ((@page - 1) * @per_page) + 1
     @end_count = [@page * @per_page, @total_count].min
     @defects = @defects.offset((@page - 1) * @per_page).limit(@per_page)
+    @defects = @defects.where(retest_count: params[:retest_count]) if params[:retest_count].present?
 
     # Compute reopened count per defect for the defects on the current page
     defect_ids_for_page = @defects.map(&:id)
