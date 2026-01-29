4157f1799dbbfeb5dbbccb87dbd1c5a4fa7c8ad9
ger619 <abolger254@gmail.com>
2026-01-29 12:57:24 +0300

    feat: Add CSV download functionality for ticket details

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -63,6 +63,9 @@ class HomeController < ApplicationController
                                         else
                                           {}
                                         end
+      @total_breached_tickets = Project.joins(tickets: %i[statuses users sla_tickets])
+                                    .where.not(statuses: { name: %w[Closed Resolved Declined] })
+                                       .where(sla_tickets: { sla_resolution_deadline: 'Breached' }).count
 
       # Count all tickets per project (duplicate, can be removed)
       @tickets_count_per_project = current_user.projects
