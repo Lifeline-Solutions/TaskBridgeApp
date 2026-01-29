1c87af44b495e14730a79bf848dca6f27aaac636
ger619 <abolger254@gmail.com>
2026-01-29 15:18:06 +0300

    feat: Update total breached tickets calculation to group by project ID

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -64,11 +64,12 @@ class HomeController < ApplicationController
                                           {}
                                         end
 
-      @total_breached_tickets = Project.joins(tickets: %i[statuses users sla_tickets])
-                                    .where.not(statuses: { name: %w[Closed Resolved Declined] })
-                                       .where(sla_tickets: { sla_resolution_deadline: 'Breached' })
-                                  .group('projects.title')
-                                  .count
+      @total_breached_tickets_per_project = Project.joins(tickets: %i[statuses users sla_tickets])
+                                                   .where.not(statuses: { name: %w[Closed Resolved Declined] })
+                                                   .where(sla_tickets: { sla_resolution_deadline: 'Breached' })
+                                                   .group('projects.id')
+                                                   .count('DISTINCT tickets.id')
+
 
       # Count all tickets per project (duplicate, can be removed)
       @tickets_count_per_project = current_user.projects
