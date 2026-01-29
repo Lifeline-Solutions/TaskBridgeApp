cfa8d3290a35e6c681188b5544d9472e007209ee
ger619 <abolger254@gmail.com>
2026-01-29 14:51:39 +0300

    feat: Update ticket breach count to group by project title

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -65,7 +65,9 @@ class HomeController < ApplicationController
                                         end
       @total_breached_tickets = Project.joins(tickets: %i[statuses users sla_tickets])
                                     .where.not(statuses: { name: %w[Closed Resolved Declined] })
-                                       .where(sla_tickets: { sla_resolution_deadline: 'Breached' }).count
+                                       .where(sla_tickets: { sla_resolution_deadline: 'Breached' })
+                                  .group('projects.title')
+                                  .count
 
       # Count all tickets per project (duplicate, can be removed)
       @tickets_count_per_project = current_user.projects
